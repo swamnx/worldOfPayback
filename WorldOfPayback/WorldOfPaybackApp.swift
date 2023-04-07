b53241c90a725adb036fb5529266cf8be1dfcf45
Vlad Sobchuk <vladsobchuk@gmail.com>
2023-04-07 14:55:32 +0200

    Initial Commit

---
+++ WorldOfPayback/WorldOfPaybackApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldOfPaybackApp.swift
+//  WorldOfPayback
+//
+//  Created by swamnx on 7.04.23.
+//
+
+import SwiftUI
+
+@main
+struct WorldOfPaybackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
